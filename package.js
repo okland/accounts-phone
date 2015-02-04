ceab70abcbdb787b1e447e4fab7499842c778d1e
okland <omri.klinger@gmail.com>
2015-02-04 11:44:19 +0200

    fix package description

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
     name         : 'okland:accounts-phone',
     version      : '0.0.1',
     // Brief, one-line summary of the package.
-    summary      : 'A login service based on mobile phone number for Metoer',
+    summary      : 'A login service based on mobile phone number for Meteor',
     // URL to the Git repository containing the source code for this package.
-    git          : '',
+    git          : 'https://github.com/okland/accounts-phone',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
