add00192967c4c57b255d32039110e4500e7fbbd
Tabrez Ahmed <tabrez.tk@gmail.com>
2015-10-25 06:10:01 +0530

    Added basic functionality to change phone number.

--- phone_client.js
+++ phone_client.js
@@ -185,13 +185,6 @@ Accounts.isPhoneVerified = function () {
     return !!(me && me.phone && me.phone.verified);
 };
 
-Accounts.changeUserPhone = function (newPhone, callback) {
-    Meteor.call('changeUserPhone',newPhone, function(error,result){
-        callback(error,result);
-    })
-};
-
-
 
 Accounts.changeUserPhone = function (newPhone, callback) {
     Meteor.call('changeUserPhone',newPhone, function(error,result){
