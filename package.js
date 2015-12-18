6e8116ab5b84535531eb43e0bcc29ade5b0ec030
veeramarni <marni.srikanth@gmail.com>
2015-12-18 17:41:43 -0500

    Update package.js
    To see the readme file in atmospherejs

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
     git          : 'https://github.com/okland/accounts-phone',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
-    documentation: null
+    documentation: 'README.md'
 });
 
 Npm.depends({
