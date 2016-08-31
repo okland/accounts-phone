400b9c837f5b87bf7745bc2bca6ff1e7385ab610
Ankit Verma <ankitv89@gmail.com>
2016-08-31 18:46:30 +0530

    Updated name and version

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-    name         : 'okland:accounts-phone',
-    version      : '0.0.21',
+    name         : 'ankitv89:accounts-phone',
+    version      : '0.0.23',
     // Brief, one-line summary of the package.
     summary      : 'A login service based on mobile phone number, For Meteor.',
     // URL to the Git repository containing the source code for this package.
-    git          : 'https://github.com/okland/accounts-phone',
+    git          : 'https://github.com/ankitv89/accounts-phone',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
